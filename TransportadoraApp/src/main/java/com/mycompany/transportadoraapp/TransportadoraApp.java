da7afc5dab822676be7cb38f3c040d4fb2b17f78
Sérgio Eduardo <sergioedumusico15@gmail.com>
2025-11-27 12:44:25 -0300

    Estrutura inicial do projeto NetBeans (MVC)

---
+++ TransportadoraApp/src/main/java/com/mycompany/transportadoraapp/TransportadoraApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.transportadoraapp;
+
+/**
+ *
+ * @author Sérgio
+ */
+public class TransportadoraApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
