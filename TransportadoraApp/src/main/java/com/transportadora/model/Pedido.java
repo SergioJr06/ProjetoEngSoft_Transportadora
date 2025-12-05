c872dbb62e86ee5b2938a45cf9ef3339676ef2ac
michel <michelpcarvalho79@gmail.com>
2025-12-05 20:27:50 -0300

    feat(pedido): Implementaçao mock do calculaFrete (RF006)

--- TransportadoraApp/src/main/java/com/transportadora/model/Pedido.java
+++ TransportadoraApp/src/main/java/com/transportadora/model/Pedido.java
@@ -33,4 +33,25 @@ public class Pedido {
         return this.peso <= this.veiculo.getCapacidadeCarga();
     }
     
+    public double calcularFrete(String cepDestino) {
+    
+    
+    final double DISTANCIA_VIAGEM_KM = 450.0; // Quantidade de KM
+    final double TARIFA_FIXA = 150.0;       // Taxa inicial por pedido
+    final double CUSTO_POR_KM = 0.85;       // Custo de 85 centavos por KM
+    final double CUSTO_POR_PESO_KG = 0.40;  // Custo de 40 centavos por KG
+    
+    // 1. Cálculo do Custo por Distância
+    double custoDistancia = DISTANCIA_VIAGEM_KM * CUSTO_POR_KM;
+    
+    // 2. Cálculo do Custo por Peso
+    // (O atributo 'this.peso' é o peso do pedido atual na classe Pedido)
+    double custoPeso = this.peso * CUSTO_POR_PESO_KG; 
+    
+  
+    double freteTotal = TARIFA_FIXA + custoDistancia + custoPeso;
+    
+    return freteTotal;
+}
+    
 }
